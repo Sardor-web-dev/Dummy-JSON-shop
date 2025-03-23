556c8c575b96ff990bdc5e79950b1ce26ce5bd32
SarikViper <dzamolovsardor5@gmail.com>
2025-03-23 21:47:27 +0500

    -

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { ProductProvider } from "./contexts/ProductContext";
 import Layout from "./layouts/Layout";
 
 function App() {
+
   return (
     <ProductProvider>
         <Layout />
     </ProductProvider>
   );
+  
 }
 
 export default App;
